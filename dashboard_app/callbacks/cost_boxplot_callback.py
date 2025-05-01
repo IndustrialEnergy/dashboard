5948b731c8a3e2cfb342059bdc9c3eb8ff481b8c
oksanaprotsukha <ok.protsukha@gmail.com>
2025-04-30 20:52:47 -0700

    (callbacks) Standardized callback for cost_boxplot chart [dashboard_app/callbacks/cost_boxplot_callback.py]

--- dashboard_app/callbacks/cost_boxplot_callback.py
+++ dashboard_app/callbacks/cost_boxplot_callback.py
@@ -12,18 +12,22 @@ def cost_boxplot_callback(app, boxplot_cost_df):
         Input("impstatus-filter", "value"),
         Input("arc-filter", "value"),
         Input("state-filter", "value"),
-        Input("outlier-filter", "value")
+        Input("outlier-filter", "value"),
     )
     def update_outputs(sector, fy_range, impstatus, arc2, state, remove_outliers):
         # create a mask for each filter
         mask = pd.Series(True, index=boxplot_cost_df.index)
-        dummy_df = boxplot_cost_df[(boxplot_cost_df['state'] == 'TX') & (boxplot_cost_df['arc2'] == '2.7492')]
-    
+        dummy_df = boxplot_cost_df[
+            (boxplot_cost_df["state"] == "TX") & (boxplot_cost_df["arc2"] == "2.7492")
+        ]
+
         if sector:
             mask &= boxplot_cost_df["sector"].isin(sector)
         if fy_range:  # Handling range slider correctly
             min_year, max_year = fy_range  # Unpack the range values
-            mask &= (boxplot_cost_df["fy"] >= min_year) & (boxplot_cost_df["fy"] <= max_year)
+            mask &= (boxplot_cost_df["fy"] >= min_year) & (
+                boxplot_cost_df["fy"] <= max_year
+            )
         if impstatus:
             mask &= boxplot_cost_df["impstatus"].isin(impstatus)
         if arc2:
@@ -35,7 +39,7 @@ def cost_boxplot_callback(app, boxplot_cost_df):
         dff = boxplot_cost_df[mask]
 
         if remove_outliers:
-            dff = filter_outliers(dff,"impcost_adj", std_threshold=2)
+            dff = filter_outliers(dff, "impcost_adj", std_threshold=2)
 
         # [TEST - REMOVE BEFORE PROD] print filtered data info
         print(f"dummy: {dummy_df.head(30)}")
@@ -43,6 +47,5 @@ def cost_boxplot_callback(app, boxplot_cost_df):
         # print(f"Filtered unique statuses: {dff['impstatus'].unique()}")
         print(f"Filtered data shape: {dff.shape}")
         print(dff.head(30))
-        
 
         return create_boxplot_cost_chart(dff)
